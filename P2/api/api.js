b51248d51c629038506b196439c5b81033669b41
Sergio Julio Garcia Urdiales <sjgu1@alu.ua.es>
2018-03-23 12:36:35 +0100

    Ejercicios 1 y 2 de prueba, P3

--- P2/api/api.js
+++ P2/api/api.js
@@ -94,22 +94,6 @@ http.createServer(app).listen(app.get('port'), function () {
     /********************************************************************************************************************************************/
     /************************************************************VALIDAR IBAN********************************************************************/
     /********************************************************************************************************************************************/
-    function modulo97(iban) {
-        var parts = Math.ceil(iban.length / 7);
-        var remainer = "";
-
-        for (var i = 1; i <= parts; i++) {
-            remainer = String(parseFloat(remainer + iban.substr((i - 1) * 7, 7)) % 97);
-        }
-
-        return remainer;
-    }
-
-    function getnumIBAN(letra) {
-        ls_letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        return ls_letras.search(letra) + 10;
-    }
-
     app.get("/validarIBAN/:iban", function (req, res) {
         var key = req.headers.restkey;
         if (connection) {
@@ -130,60 +114,6 @@ http.createServer(app).listen(app.get('port'), function () {
                                 return res.status(200).send({ correcto: true, status: 200, message: "El IBAN es válido" });
                             } else {
                                 return res.status(200).send({ correcto: false, status: 200, message: "El IBAN NO es válido" });
-                            }
-                        }
-                    }
-                });
-            } catch (err) {
-                return res.status(500).send({ correcto: false, status: 500, message: "Problemas con el servidor o la base de datos" });
-
-            }
-        } else {
-            return res.status(500).send({ correcto: false, status: 500, message: "Problemas con el servidor o la base de datos" });
-        }
-    });
-
-    /********************************************************************************************************************************************/
-    /************************************************************VALIDAR DNI*********************************************************************/
-    /********************************************************************************************************************************************/
-
-    app.get("/validarNIF/:nif", function (req, res) {
-        var key = req.headers.restkey;
-        if (connection) {
-            try {
-                connection.query("SELECT count(1) AS recuento from soap where clave like '" + key + "' ;", function (error, rows) {
-                    if (error) {
-                        console.log(error);
-                        return res.status(500).send({ correcto: false, status: 500, message: "Problemas con el servidor o la base de datos" });
-                    } else {
-                        if (rows[0].recuento == 0) {
-                            return res.status(401).send({ correcto: false, status: 401, message: "RestKey incorrecta" });
-                        } else {
-                            var dni = req.params.nif
-                            var numero, let1, letra;
-                            var expresion_regular_dni = /^[XYZ]?\d{5,8}[A-Z]$/;
-
-                            dni = dni.toUpperCase();
-
-                            if (expresion_regular_dni.test(dni) === true) {
-                                numero = dni.substr(0, dni.length - 1);
-                                numero = numero.replace('X', 0);
-                                numero = numero.replace('Y', 1);
-                                numero = numero.replace('Z', 2);
-                                let1 = dni.substr(dni.length - 1, 1);
-                                numero = numero % 23;
-                                letra = 'TRWAGMYFPDXBNJZSQVHLCKET';
-                                letra = letra.substring(numero, numero + 1);
-                                if (letra != let1) {
-                                    //alert('Dni erroneo, la letra del NIF no se corresponde');
-                                    return res.status(200).send({ correcto: false, status: 200, message: "El NIF no es válido" });
-                                } else {
-                                    //alert('Dni correcto');
-                                    return res.status(200).send({ correcto: true, status: 200, message: "El NIF es válido" });
-                                }
-                            } else {
-                                //alert('Dni erroneo, formato no válido');
-                                return res.status(200).send({ correcto: false, status: 200, message: "El NIF no es válido, formato erroneo" });
                             }
                         }
                     }
@@ -228,7 +158,7 @@ http.createServer(app).listen(app.get('port'), function () {
                             connection.query("select cod_postal, poblacion, provincia from poblaciones INNER JOIN provincias ON provincias.cod_prov=poblaciones.cod_prov where cod_postal = " + cp + " LIMIT 1;", function (error, rows) {
                                 if (rows.length >= 1) {
                                     return res.status(200).send({
-                                        correcto: false,
+                                        correcto: true,
                                         status: 200,
                                         message: "Se han encontrado datos sobre el código consultado",
                                         Poblacion:
@@ -247,7 +177,7 @@ http.createServer(app).listen(app.get('port'), function () {
                                         message: "No existen datos sobre el código consultado",
                                         Poblacion:
                                             {
-                                                codigoPostal: null,
+                                                codigoPostal: req.params.cp,
                                                 poblacion: null,
                                                 provincia: null,
                                                 existe: false
@@ -296,6 +226,7 @@ http.createServer(app).listen(app.get('port'), function () {
 
     app.post("/generarPresupuesto", function (req, res) {
         var key = req.headers.restkey;
+        console.log(req.body)
         if (connection) {
             try {
                 connection.query("SELECT count(1) AS recuento from soap where clave like '" + key + "' ;", function (error, rows) {
@@ -324,6 +255,18 @@ http.createServer(app).listen(app.get('port'), function () {
                                     }
                             });
                         } else {
+                            if( req.body.fechaPresupuesto == "" ||req.body.idcliente == "" ||req.body.referenciaProducto == "" ||req.body.cantidadProducto == "" ){
+                                return res.status(500).send({
+                                    "correcto" : false,
+                                    "status" : 500,
+                                    "message" : "Problema a la hora de crear el recurso",
+                                    "Presupuesto" : 
+                                      {
+                                        "idPresupuesto": false,
+                                        "presupuestoGeneradoCorrectamente" : false
+                                      }
+                                  });
+                            }
                             var values = "('" + req.body.fechaPresupuesto + "', '" + req.body.idCliente + "', '" + req.body.referenciaProducto + "', '" + req.body.cantidadProducto + "')";
                             connection.query("INSERT INTO presupuestos (fechaPresupuesto, idCliente, referenciaProducto, cantidadProducto) VALUES " + values + ";", function (error2, rows) {
                                 console.log(rows)
